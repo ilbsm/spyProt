7e2c88f8e136f01cb14c337bdb4c7c971fafa465
Paweł Rubach <pawel.rubach@gmail.com>
2021-03-30 20:17:04 +0200

    Bumped version to 0.4.2

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="spyprot",
-    version='0.4.0',
+    version='0.4.2',
     author="INTERDISCIPLINARY LABORATORY of BIOLOGICAL SYSTEMS MODELLING, University of Warsaw, Warsaw, Poland",
     author_email="bmjastrzebski@gmail.com, p.rubach@cent.uw.edu.pl",
     description="This package provides a set of tools for accessing protein databases and manipulating PDB/CIF files.",
@@ -17,7 +17,7 @@ setup(
     install_requires=[
         'tqdm>=4.31.1', 'lxml>=4.5.0', 'requests>=2.0.0', 'biopython>1.60', 'psutil>5.6.0', 'subprocess32>=3.5.0', 'wget>=3.0', 'mysolr>=0.8.3'
     ],
-    python_requires='>=3.5.0',
+    python_requires='>=3.6.0',
     classifiers=[
         "Programming Language :: Python :: 3",
         'Programming Language :: Python :: 3.6',
